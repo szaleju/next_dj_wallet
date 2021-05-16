18bd687dc82b4f009b13ac9550b298b2e8e1eef2
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-16 10:57:29 +0200

    transaction views

--- wallet/views.py
+++ wallet/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from .models import Wallet, Transaction
+from .models import Wallet, Transaction, Asset
 from .serializers import UserSerializer, WalletSerializer, TransactionSerializer
 from rest_framework import generics, permissions
 from rest_framework.response import Response
@@ -35,14 +35,27 @@ def ListTransactions(request):
 @permission_classes([IsAuthenticated])
 def CreateTransaction(request):
     user = request.user
+    assets = Asset.objects.all()
     data = request.data
-    print("DATA", data)
-    transaction = Transaction.objects.create(
-        user=user,
-        asset_id=data['asset'],
-        quantity=data['quantity'],
-        transaction_type='transfer',
-        price=10
-    )
+
+    for ass in assets:
+        if data['asset'].strip() == str(ass):
+            print("ZNALAZLEM", ass)
+
+    if data['asset'] not in assets:
+        print('KURWA GON PRZYWIEŹLI')
+        asset = Asset.objects.create(
+            ticker='nowy',
+            name=data['asset'],
+        )
+        print('ASSET', asset)
+    else:
+        transaction = Transaction.objects.create(
+            user=user,
+            asset_id=data['asset'],
+            quantity=data['quantity'],
+            transaction_type='transfer',
+            price=10
+        )
     # dodaj przypadek nowego aktywa
     return Response('transaction created')
